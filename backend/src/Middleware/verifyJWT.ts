5e50e7af1843b507544c5db0094cae19d3aff4c2
James <jamestariga08@live.com>
2022-11-13 21:29:08 -0800

    🔥 Created a middleware to verify jwt

---
+++ backend/src/Middleware/verifyJWT.ts
@@ -0,0 +1,34 @@
+import * as JWT from 'jsonwebtoken'
+import { Request, Response, NextFunction } from 'express'
+import { Users } from '../Model/User'
+import { Roles } from '../Configs/rolesList'
+
+interface RequestWithUser extends Request {
+  user: Users
+  roles: Roles[]
+}
+
+const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization
+
+  const secret = `${process.env.ACCESS_TOKEN_SECRET}`
+
+  if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401)
+
+  const token = authHeader.split(' ')[1]
+  JWT.verify(token, secret, (err: any, decoded: any) => {
+    if (err) {
+      return res.sendStatus(403)
+    }
+
+    const verifyRequest = req as RequestWithUser
+
+    verifyRequest.user = decoded.UserInfo.userName
+
+    verifyRequest.roles = decoded.UserInfo.roles
+
+    next()
+  })
+}
+
+export default verifyJWT
