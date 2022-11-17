cdb4542d316cf9868797479397ad04769174e3e0
James <jamestariga08@live.com>
2022-11-16 20:57:07 -0800

    🔥 Created a controller that will handle the logout

---
+++ backend/src/Controller/logoutController.ts
@@ -0,0 +1,39 @@
+import User, { Users } from '../Model/User'
+import { Request, Response } from 'express'
+
+export const handleLogout = async (req: Request, res: Response) => {
+  const cookies = req.cookies
+
+  if (!cookies.jwt) return res.sendStatus(204)
+
+  const refreshToken = cookies.jwt
+
+  // Finding a user in the database with the refreshToken that is in the cookie
+  const foundUser: Users | null = await User.findOne({
+    refreshToken: refreshToken,
+  }).exec()
+
+  if (!foundUser) {
+    res.clearCookie('jwt', {
+      httpOnly: true,
+      sameSite: 'none',
+      // Add secure: true when using https
+      // secure: true,
+    })
+    return res.sendStatus(204)
+  }
+
+  // Clear the refresh token from the database
+  foundUser.refreshToken = ''
+  const result = await foundUser.save()
+  console.log(result)
+
+  res.clearCookie('jwt', {
+    httpOnly: true,
+    sameSite: 'none',
+    // Add secure: true when using https
+    // secure: true,
+  })
+
+  res.sendStatus(204)
+}
