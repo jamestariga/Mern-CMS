e74facd722d621f2282ce450485bc86f4cd8a795
James <jamestariga08@live.com>
2022-12-03 18:37:56 -0800

    🔥 Added new routes and middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,18 @@ dotenv.config()
 import express, { Express, Request, Response } from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import credentials from './Middleware/credentials'
 import corsOption from './Configs/corsOptions'
 import connectDB from './Configs/dbConn'
 import user from './Routes/api/users'
+import ticket from './Routes/api/tickets'
+import product from './Routes/api/product'
+import register from './Routes/register'
+import auth from './Routes/auth'
+import logout from './Routes/logout'
+import refresh from './Routes/refresh'
+import verifyJWT from './Middleware/verifyJWT'
 
 const PORT = process.env.PORT || 3001
 
@@ -21,8 +29,11 @@ app.use(credentials)
 app.use(cors(corsOption))
 
 // Body parser middleware
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+// Cookie parser middleware
+app.use(cookieParser())
 
 app.get('/', (req: Request, res: Response) => {
   res.send({
@@ -30,7 +41,16 @@ app.get('/', (req: Request, res: Response) => {
   })
 })
 
+app.use('/register', register)
+app.use('/auth', auth)
+app.use('/logout', logout)
+app.use('/refresh', refresh)
+
+// app.use(verifyJWT)
+
 app.use('/user', user)
+app.use('/ticket', ticket)
+app.use('/product', product)
 
 mongoose.connection.once('open', () => {
   console.log('Connected to database')
