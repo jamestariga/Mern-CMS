a66c9611a9d996da928fb6bffcef6730d95a5700
James <jamestariga08@live.com>
2022-12-14 22:27:50 -0800

    🔥 Initialized react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,14 @@
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import Dog from './components/Dog'
-import Populations from './components/Populations'
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom'
+import Layout from '@/components/Layout'
+import Dog from '@/components/Dog'
+import Populations from '@/components/Populations'
+import Content from '@/components/Content'
 
 const App = () => {
   const queryClient = new QueryClient({
@@ -11,12 +19,19 @@ const App = () => {
     },
   })
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Content />} />
+        <Route path='/dog' element={<Dog />} />
+        <Route path='/population' element={<Populations />} />
+      </Route>
+    )
+  )
+
   return (
     <QueryClientProvider client={queryClient}>
-      <div>
-        {/* <Dog /> */}
-        <Populations />
-      </div>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   )
 }
