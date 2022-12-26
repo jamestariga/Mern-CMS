054330954b834ade2f6b8fe4983563f593018ee0
James <jamestariga08@live.com>
2022-12-25 22:31:41 -0800

    🔥 Minor change

--- backend/src/Middleware/credentials.ts
+++ backend/src/Middleware/credentials.ts
@@ -4,7 +4,7 @@ const credentials = (req: any, res: any, next: any) => {
   const origin = req.headers.origin
 
   if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', true)
+    res.setHeader('Access-Control-Allow-Credentials', true)
   }
 
   next()
