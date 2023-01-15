34d69c06203658719c2b904b2ffd30d20f02ad49
James <jamestariga08@live.com>
2023-01-14 18:20:45 -0800

    🔥 Added a toggle button to show passord and fixed the layout

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -4,6 +4,7 @@ import { useMutation } from '@tanstack/react-query'
 import { USER_REGEX, PWD_REGEX } from '@/utils/regex'
 import cmsApi from '@/api/cmsApi'
 import { registerResponse, registerUser } from '@/types/types'
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'
 
 const registerFn = async (props: registerUser) => {
   const { userName, password, firstName, lastName } = props
@@ -30,6 +31,7 @@ const Register = () => {
   const [password, setPassword] = useState<string>('')
   const [validPassword, setValidPassword] = useState<boolean>(false)
   const [passwordFocus, setPasswordFocus] = useState<boolean>(false)
+  const [showPassword, setShowPassword] = useState<boolean>(false)
 
   const [firstName, setFirstName] = useState<string>('')
 
@@ -38,6 +40,7 @@ const Register = () => {
   const [matchPassword, setMatchPassword] = useState<string>('')
   const [validMatchPassword, setValidMatchPassword] = useState<boolean>(false)
   const [matchPasswordFocus, setMatchPasswordFocus] = useState<boolean>(false)
+  const [showMatchPassword, setShowMatchPassword] = useState<boolean>(false)
 
   const [error, setError] = useState<string>('')
 
@@ -83,6 +86,24 @@ const Register = () => {
     setValidMatchPassword(match)
   }, [password, matchPassword])
 
+  const togglePassword = () => {
+    setShowPassword((showPassword) => {
+      if (typeof showPassword === 'boolean') {
+        return !showPassword
+      }
+      return showPassword
+    })
+  }
+
+  const toggleMatchPassword = () => {
+    setShowMatchPassword((showMatchPassword) => {
+      if (typeof showMatchPassword === 'boolean') {
+        return !showMatchPassword
+      }
+      return showMatchPassword
+    })
+  }
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
@@ -110,94 +131,141 @@ const Register = () => {
   }
 
   return (
-    <header>
-      <h1>Register</h1>
+    <header className='flex flex-col items-center justify-center h-screen'>
       <form
         onSubmit={handleSubmit}
-        className='flex flex-col justify-center items-center space-y-4'
+        className='flex flex-col items-center justify-center w-72 sm:w-96'
       >
-        <div>
-          <label htmlFor='username'>Username</label>
-          <input
-            type='text'
-            id='username'
-            name='username'
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            onFocus={() => setUserFocus(true)}
-            onBlur={() => setUserFocus(false)}
-            ref={userRef}
-          />
-          {userFocus && !validUserName && (
-            <p>Username must be at least 6 characters long</p>
-          )}
+        <h1 className='text-3xl font-bold'>Register</h1>
+        <div className='flex flex-col items-center justify-center w-full mt-4'>
+          <div className='flex flex-col items-center justify-center w-full'>
+            <label htmlFor='username' className='text-sm font-bold'>
+              Username
+            </label>
+            <input
+              type='text'
+              id='username'
+              name='username'
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              onFocus={() => setUserFocus(true)}
+              onBlur={() => setUserFocus(false)}
+              ref={userRef}
+              className='w-full px-2 py-1 mt-1 border border-gray-300 rounded focus:outline-none focus:border-blue-300'
+            />
+            {userFocus && !validUserName && (
+              <p>Username must be at least 6 characters long</p>
+            )}
+          </div>
         </div>
-        <div>
-          <label htmlFor='firstName'>First Name</label>
-          <input
-            type='text'
-            id='firstName'
-            name='firstName'
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
+        <div className='flex flex-col items-center justify-center w-full mt-4'>
+          <div className='flex flex-col items-center justify-center w-full'>
+            <label htmlFor='firstName'>First Name</label>
+            <input
+              type='text'
+              id='firstName'
+              name='firstName'
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              className='w-full px-2 py-1 mt-1 border border-gray-300 rounded focus:outline-none focus:border-blue-300'
+            />
+          </div>
         </div>
-        <div>
-          <label htmlFor='lastName'>Last Name</label>
-          <input
-            type='text'
-            id='lastName'
-            name='lastName'
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
+        <div className='flex flex-col items-center justify-center w-full mt-4'>
+          <div className='flex flex-col items-center justify-center w-full'>
+            <label htmlFor='firstName'>Last Name</label>
+            <input
+              type='text'
+              id='lastName'
+              name='lastName'
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              className='w-full px-2 py-1 mt-1 border border-gray-300 rounded focus:outline-none focus:border-blue-300'
+            />
+          </div>
         </div>
-        <div className='flex'>
-          <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            id='password'
-            name='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            onFocus={() => setPasswordFocus(true)}
-            onBlur={() => setPasswordFocus(false)}
-            ref={passwordRef}
-          />
-          {passwordFocus && !validPassword && (
-            <p>
-              Password must be at least 8 characters long, contain at least one
-              uppercase letter, one lowercase letter, one number, and one
-              special character
-            </p>
-          )}
+        <div className='flex flex-col items-center justify-center w-full mt-4'>
+          <div className='flex flex-col items-center justify-center w-full'>
+            <label htmlFor='password'>Password</label>
+            <div className='w-full flex items-center'>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                id='password'
+                name='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onFocus={() => setPasswordFocus(true)}
+                onBlur={() => setPasswordFocus(false)}
+                ref={passwordRef}
+                className='w-full px-2 py-1 mt-1 border border-gray-300 rounded focus:outline-none focus:border-blue-300'
+              />
+              <button
+                type='button'
+                className='absolute w-8 h-8 translate-x-[16rem] sm:translate-x-[22rem]'
+                onClick={togglePassword}
+              >
+                {showPassword ? (
+                  <AiFillEyeInvisible className='w-6 h-6 text-blue-500' />
+                ) : (
+                  <AiFillEye className='w-6 h-6 text-blue-500' />
+                )}
+              </button>
+            </div>
+            {passwordFocus && !validPassword && (
+              <p className='mt-4 font-bold text-sm'>
+                Password must be at least 8 characters long, contain at least
+                one uppercase letter, one lowercase letter, one number, and one
+                special character
+              </p>
+            )}
+          </div>
         </div>
-        <div>
-          <label htmlFor='matchPassword'>Confirm Password</label>
-          <input
-            type='password'
-            id='matchPassword'
-            name='matchPassword'
-            value={matchPassword}
-            onChange={(e) => setMatchPassword(e.target.value)}
-            onFocus={() => setMatchPasswordFocus(true)}
-            onBlur={() => setMatchPasswordFocus(false)}
-          />
-          {matchPasswordFocus && !validMatchPassword && (
-            <p>Passwords do not match</p>
-          )}
-        </div>
-        <button type='submit' disabled={mutate.isLoading}>
-          {mutate.isLoading ? 'Loading...' : 'Register'}
-        </button>
-        <div
-          tabIndex={-1}
-          ref={errRef}
-          aria-live='polite'
-          aria-atomic='true'
-          aria-relevant='all'
-        >
-          {error}
+        <div className='flex flex-col items-center justify-center w-full mt-4'>
+          <div className='flex flex-col items-center justify-center w-full'>
+            <label htmlFor='matchPassword'>Confirm Password</label>
+            <div className='w-full flex items-center'>
+              <input
+                type={showMatchPassword ? 'text' : 'password'}
+                id='matchPassword'
+                name='matchPassword'
+                value={matchPassword}
+                onChange={(e) => setMatchPassword(e.target.value)}
+                onFocus={() => setMatchPasswordFocus(true)}
+                onBlur={() => setMatchPasswordFocus(false)}
+                className='w-full px-2 py-1 mt-1 border border-gray-300 rounded focus:outline-none focus:border-blue-300'
+              />
+              <button
+                type='button'
+                className='absolute w-8 h-8 translate-x-[16rem] sm:translate-x-[22rem]'
+                onClick={toggleMatchPassword}
+              >
+                {showMatchPassword ? (
+                  <AiFillEyeInvisible className='w-6 h-6 text-blue-500' />
+                ) : (
+                  <AiFillEye className='w-6 h-6 text-blue-500' />
+                )}
+              </button>
+            </div>
+            {matchPasswordFocus && !validMatchPassword && (
+              <p className='mt-4 font-bold text-sm'>Passwords do not match</p>
+            )}
+          </div>
+          <button
+            type='submit'
+            disabled={mutate.isLoading}
+            className='w-full px-4 py-2 mt-4 text-white bg-blue-500 rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600'
+          >
+            {mutate.isLoading ? 'Loading...' : 'Register'}
+          </button>
+          <div
+            ref={errRef}
+            className={`flex items-center justify-center w-full mt-4 text-sm font-bold ${
+              error ? 'text-red-500' : 'text-green-500'
+            }`}
+          >
+            {error}
+          </div>
+          <Link to='/login'>Already have an account?</Link>
         </div>
       </form>
     </header>
