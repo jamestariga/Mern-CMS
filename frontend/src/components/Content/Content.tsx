2b05f2ec2d64c3a2aea99a920e12f7ed8585fbf4
James <jamestariga08@live.com>
2023-02-04 22:45:04 -0800

    🔥 Updated the content component

--- frontend/src/components/Content/Content.tsx
+++ frontend/src/components/Content/Content.tsx
@@ -1,23 +1,30 @@
 import { IContent, IProduct } from '@/types/types'
+import { Card, Button } from 'react-daisyui'
 
 const Content = (props: IContent) => {
   const { data, title } = props
 
   return (
-    <header className='flex flex-col items-center justify-center px-4'>
-      <h1>{title}</h1>
-      {data?.map((item: IProduct) => (
-        <div
-          key={item._id}
-          className='flex flex-col justify-center items-center'
-        >
-          <h1>{item.name}</h1>
-          <p>{item.price}</p>
-          <p>{item.description}</p>
-          <img src={item.image.url} alt='' />
-        </div>
-      ))}
-    </header>
+    <>
+      <h1 className='text-xl font-bold pb-10'>{title}</h1>
+      <div className='grid grid-cols-2 xl:grid-cols-4 items-center justify-center gap-4 px-10'>
+        {data?.map((item: IProduct) => (
+          <Card key={item._id} bordered={false}>
+            <Card.Image src={item.image.url} className='' />
+            <Card.Body className='items-center text-center'>
+              <Card.Title>{item.name}</Card.Title>
+              <p className='text-base'>{item.description}</p>
+              <p className='text-base font-semibold'>{item.price}</p>
+              <Card.Actions>
+                <Button className='btn bg-blue-700 text-white border-none hover:bg-blue-600'>
+                  Buy Now
+                </Button>
+              </Card.Actions>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </>
   )
 }
 
