5a13546fbf30bbc759fb24610a22ae4d64ed269b
James <jamestariga08@live.com>
2023-02-08 23:26:49 -0800

    🔥 Updated the layout

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,9 +2,18 @@ import { useQuery } from '@tanstack/react-query'
 import { Product } from '../../types/types'
 import useAxiosPrivate from '@/hooks/useAxiosPrivate'
 import Content from '@/components/Content'
+import { useNavigate } from 'react-router-dom'
+import useLogout from '@/hooks/useLogout'
 
 const Home = () => {
   const axiosPrivate = useAxiosPrivate()
+  const navigate = useNavigate()
+  const logout = useLogout()
+
+  const handleLogout = async () => {
+    await logout()
+    navigate('/login')
+  }
 
   const getProducts = async () => {
     const response = await axiosPrivate.get<Product>('/publicProducts')
@@ -24,7 +33,13 @@ const Home = () => {
 
   return (
     <>
-      <Content data={productData} title='Public Products' />
+      <header className='flex flex-col items-center justify-center'>
+        <Content data={productData} title='Public Products' />
+        <div className='flex flex-col justify-center'>
+          <button onClick={() => navigate('/admin')}>Admin</button>
+          <button onClick={handleLogout}>Sign out</button>
+        </div>
+      </header>
     </>
   )
 }
