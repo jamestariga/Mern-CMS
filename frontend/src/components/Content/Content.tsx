3915c670ac84eed2f7d8e7729fa7ac97956a513d
James <jamestariga08@live.com>
2023-02-16 22:24:03 -0800

    🔥 Added a link for each card

--- frontend/src/components/Content/Content.tsx
+++ frontend/src/components/Content/Content.tsx
@@ -1,5 +1,6 @@
 import { IContent, IProduct } from '@/types/types'
 import { Card, Button } from 'react-daisyui'
+import { Link } from 'react-router-dom'
 
 const Content = (props: IContent) => {
   const { data, title } = props
@@ -9,19 +10,21 @@ const Content = (props: IContent) => {
       <h1 className='text-xl font-bold pb-10'>{title}</h1>
       <div className='grid grid-cols-2 xl:grid-cols-4 items-center justify-center gap-4 px-10'>
         {data?.map((item: IProduct) => (
-          <Card key={item._id} bordered={false}>
-            <Card.Image src={item.image.url} className='' />
-            <Card.Body className='items-center text-center'>
-              <Card.Title>{item.name}</Card.Title>
-              <p className='text-base'>{item.description}</p>
-              <p className='text-base font-semibold'>{item.price}</p>
-              <Card.Actions>
-                <Button className='btn bg-blue-700 text-white border-none hover:bg-blue-600'>
-                  Buy Now
-                </Button>
-              </Card.Actions>
-            </Card.Body>
-          </Card>
+          <Link to={`/product/${item.slug}`} key={item._id}>
+            <Card bordered={false}>
+              <Card.Image src={item.image.url} className='' />
+              <Card.Body className='items-center text-center'>
+                <Card.Title>{item.name}</Card.Title>
+                <p className='text-base'>{item.description}</p>
+                <p className='text-base font-semibold'>{item.price}</p>
+                <Card.Actions>
+                  <Button className='btn bg-blue-700 text-white border-none hover:bg-blue-600'>
+                    Buy Now
+                  </Button>
+                </Card.Actions>
+              </Card.Body>
+            </Card>
+          </Link>
         ))}
       </div>
     </>
