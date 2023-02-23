697003a3d36e73b2d8f513cb8de740722d88b89c
James <jamestariga08@live.com>
2023-02-22 23:28:01 -0800

    🔥 Updated the styles of the card

--- frontend/src/components/Content/Content.tsx
+++ frontend/src/components/Content/Content.tsx
@@ -8,23 +8,29 @@ const Content = (props: IContent) => {
   return (
     <>
       <h1 className='text-xl font-bold pb-10'>{title}</h1>
-      <div className='grid grid-cols-2 xl:grid-cols-4 items-center justify-center gap-4 px-10'>
+      <div className='grid sm:grid-cols-2 xl:grid-cols-4 items-center justify-center gap-4 xl:gap-8 px-10'>
         {data?.map((item: IProduct) => (
-          <Link to={`/product/${item.slug}`} key={item._id}>
-            <Card bordered={false}>
-              <Card.Image src={item.image.url} className='' />
-              <Card.Body className='items-center text-center'>
-                <Card.Title>{item.name}</Card.Title>
-                <p className='text-base'>{item.description}</p>
-                <p className='text-base font-semibold'>{item.price}</p>
-                <Card.Actions>
-                  <Button className='btn bg-blue-700 text-white border-none hover:bg-blue-600'>
-                    Buy Now
-                  </Button>
-                </Card.Actions>
-              </Card.Body>
-            </Card>
-          </Link>
+          <Card
+            bordered={true}
+            className='rounded-2xl overflow-hidden border-zinc-200 border-2'
+          >
+            <Link to={`/product/${item.slug}`} key={item._id}>
+              <Card.Image
+                src={item.image.url}
+                className='overflow-hidden hover:scale-[1.1] transition h-full w-full'
+              />
+            </Link>
+            <Card.Body className='items-center text-center'>
+              <Card.Title>{item.name}</Card.Title>
+              <p className='text-base'>{item.description}</p>
+              <p className='text-base font-semibold'>{`$${item.price}`}</p>
+              <Card.Actions>
+                <Button className='btn bg-blue-700 text-white border-none hover:bg-blue-600'>
+                  Add to cart
+                </Button>
+              </Card.Actions>
+            </Card.Body>
+          </Card>
         ))}
       </div>
     </>
